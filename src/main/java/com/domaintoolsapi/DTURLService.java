71c091639537ab30217e2e6814f2d2854d5d4800
jsosin <jsosin@eurodns.com>
2011-11-02 11:59:21 +0100

    Delete a println

--- src/main/java/com/domaintoolsapi/DTURLService.java
+++ src/main/java/com/domaintoolsapi/DTURLService.java
@@ -30,7 +30,6 @@ public class DTURLService {
 		addParameters(domainToolsRequest);
 		addResponseFormat(domainToolsRequest);
 		try {
-			System.out.println(string_url);
 			url = new URL(string_url);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
